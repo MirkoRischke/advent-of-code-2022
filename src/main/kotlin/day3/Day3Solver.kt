29fac7c352ff1614dc8404cdc88f02e49f4a967a
Mirko Rischke <mirko.rischke@volkswagen.de>
2022-12-04 22:01:14 +0100

    Add tests for first puzzle of day3

---
+++ src/main/kotlin/day3/Day3Solver.kt
@@ -0,0 +1,14 @@
+package day3
+
+import Solver
+import java.nio.file.Path
+
+class Day3Solver: Solver {
+    override fun solveFirstPuzzle(input: Path): Int {
+        return 0
+    }
+
+    override fun solveSecondPuzzle(input: Path): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/day3/Day3SolverTest.kt
@@ -0,0 +1,32 @@
+package day3
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import testutils.ResourceHelper
+
+class Day3SolverTest : BehaviorSpec({
+
+    val solver = Day3Solver()
+
+    Given("First puzzle") {
+        And("the sample input") {
+            val input = ResourceHelper.pathFor("/day3/example_input.txt")
+            When("I solve it") {
+                val result = solver.solveFirstPuzzle(input)
+                Then("the result is 157") {
+                    result shouldBe 157
+                }
+            }
+        }
+
+        And("my personal input") {
+            val input = ResourceHelper.pathFor("/day3/personal_input.txt")
+            When("I solve it") {
+                val result = solver.solveFirstPuzzle(input)
+                Then("the result is X") {
+                    result shouldBe 157
+                }
+            }
+        }
+    }
+})
