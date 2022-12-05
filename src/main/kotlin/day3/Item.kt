057660892aaebb4ae72bc328e137cdd51772835f
Mirko Rischke <mirko.rischke@volkswagen.de>
2022-12-05 19:22:27 +0100

    Set score of an item

--- src/main/kotlin/day3/Item.kt
+++ src/main/kotlin/day3/Item.kt
@@ -2,10 +2,24 @@ package day3
 
 data class Item(private val letter: Char) {
 
+    val score: Int
+    private val lowerCaseLetters = CharRange('a', 'z')
+    private val upperCaseLetters = CharRange('A', 'Z')
+
     init {
         if (!letter.isLetter()) {
             throw IllegalArgumentException("Only letters are allowed (a-z,A-Z)")
         }
+        score = calculateScore(letter)
     }
 
+    private fun calculateScore(char: Char): Int {
+        return when(char) {
+            in lowerCaseLetters -> lowerCaseLetters.indexOf(char) + 1
+            in upperCaseLetters -> upperCaseLetters.indexOf(char) + 27
+            else -> {
+                throw Error("calculateScore was called with a char that is not a letter. This should not happen.")
+            }
+        }
+    }
 }
--- src/test/kotlin/day3/ItemTest.kt
+++ src/test/kotlin/day3/ItemTest.kt
@@ -2,7 +2,9 @@ package day3
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldStartWith
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.char
@@ -37,5 +39,35 @@ class ItemTest : BehaviorSpec({
         }
     }
 
+    Given("I want to get the score of an item") {
+        And("I have a an item with a lowercase letter") {
+            listOf(
+                'a' to 1,
+                'e' to 5,
+                'm' to 13,
+                'z' to 26,
+            ).forEach { (letter, score) ->
 
+                val item = Item(letter)
+                Then("the score for $letter is its position in the alphabet") {
+                    item.score shouldBe score
+                }
+            }
+        }
+
+        And("I have a an item with an uppercase letter") {
+            listOf(
+                'A' to 27,
+                'E' to 31,
+                'M' to 39,
+                'Z' to 52,
+            ).forEach { (letter, score) ->
+
+                val item = Item(letter)
+                Then("the score for $letter is $score") {
+                    item.score shouldBe score
+                }
+            }
+        }
+    }
 })
