edd52a18bb111820e62d159b9ff6b8f020b91060
Mirko Rischke <mirko.rischke@volkswagen.de>
2022-12-10 12:13:05 +0100

    Create tests for day4 puzzle1

---
+++ src/main/kotlin/day4/Day4Solver.kt
@@ -0,0 +1,14 @@
+package day4
+
+import Solver
+import java.nio.file.Path
+
+class Day4Solver : Solver {
+    override fun solveFirstPuzzle(input: Path): Int {
+        return 0
+    }
+
+    override fun solveSecondPuzzle(input: Path): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/day4/Day4SolverTest.kt
@@ -0,0 +1,29 @@
+package day4
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import testutils.ResourceHelper
+
+class Day4SolverTest : DescribeSpec({
+
+    val solver = Day4Solver()
+
+    describe("puzzle 1") {
+        it("example input") {
+            val input = ResourceHelper.pathFor("/day4/example_input.txt")
+
+            val result = solver.solveFirstPuzzle(input)
+
+            result shouldBe 2
+        }
+
+        it("personal input") {
+            val input = ResourceHelper.pathFor("/day4/personal_input.txt")
+
+            val result = solver.solveFirstPuzzle(input)
+
+            result shouldBe 2
+        }
+    }
+
+})
